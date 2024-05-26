874593c0785f8b397aac25c5d2e37981c9645565
Abdul Gany Osman <Abdul.Osman@bbd.co.za>
2024-05-26 22:39:44 +0200

    Added basic frontend folder structure

---
+++ project-tracker-frontend/src/components/project-grid/project-grid.jsx
@@ -0,0 +1,5 @@
+export default function ProjectGrid() {
+    return (
+        <h1>This is where the project details will go</h1>
+    )
+}
---
+++ project-tracker-frontend/src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import HomePage from './pages/Home.jsx'
+import LoginPage from './pages/Login.jsx'
+import ErrorPage from './pages/Error.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage></HomePage>,
+    errorElement: <ErrorPage></ErrorPage>
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
