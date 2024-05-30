34b2ba6e9599fc68c2df857c77aaa14d53466152
Daniel Pienaar <daniel.pienaar@bbd.co.za>
2024-05-30 11:07:04 +0200

    added create project form

--- project-tracker-frontend/src/main.jsx
+++ project-tracker-frontend/src/main.jsx
@@ -5,6 +5,13 @@ import './index.css'
 import HomePage from './pages/Home.jsx'
 import LoginPage from './pages/Login.jsx'
 import ErrorPage from './pages/Error.jsx'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+import theme from "./theme.tsx"
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +27,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ project-tracker-frontend/src/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        background: {
+            default: 'rgb(30,30,30)'
+        }
+    }
+});
+
+export default theme;
