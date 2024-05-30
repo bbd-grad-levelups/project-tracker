ab837488243e9aadea2a9662ceab5c85ca687c83
Daniel Pienaar <daniel.pienaar@bbd.co.za>
2024-05-30 14:24:01 +0200

    added dark mode and themes

--- project-tracker-frontend/src/components/navbar/navbar.jsx
+++ project-tracker-frontend/src/components/navbar/navbar.jsx
@@ -1,9 +1,20 @@
 import './navbar.css'
+import { Switch, Button, Typography, Stack } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
-function NavBar() {
+function NavBar({ darkMode, toggleDarkTheme }) {
+
+    const theme = useTheme();
+
     return (
         <section className="navbar">
-            <button>Logout</button>
+            <Stack direction={'row'} spacing={3} alignItems={'center'} justifyContent={'center'}>
+                <Typography variant="body1">{theme.palette.mode} mode</Typography>
+                <Switch checked={darkMode} onChange={toggleDarkTheme}></Switch>
+                <Button variant="contained" color="secondary" type="submit" size="large">
+                    Logout
+                </Button>
+            </Stack>
         </section>
     )
 }
--- project-tracker-frontend/src/main.jsx
+++ project-tracker-frontend/src/main.jsx
@@ -5,9 +5,6 @@ import './index.css'
 import HomePage from './pages/Home.jsx'
 import LoginPage from './pages/Login.jsx'
 import ErrorPage from './pages/Error.jsx'
-import { ThemeProvider } from '@emotion/react'
-import { CssBaseline } from '@mui/material'
-import theme from "./theme.tsx"
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -27,9 +24,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline enableColorScheme />
       <RouterProvider router={router} />
-    </ThemeProvider>
   </React.StrictMode>,
 )
