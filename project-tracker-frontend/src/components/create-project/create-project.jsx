3cdfcaa991ab1da47cd09024fae4c7c958095b85
Daniel Pienaar <daniel.pienaar@bbd.co.za>
2024-05-31 14:47:12 +0200

    Added more create-project input fields and created popups for navbar input

--- project-tracker-frontend/src/components/create-project/create-project.jsx
+++ project-tracker-frontend/src/components/create-project/create-project.jsx
@@ -9,19 +9,41 @@ function CreateProject() {
                 <Stack spacing={3} alignItems="flex-start" component="form" action="/create" method="POST" >
                     <Typography variant="h2">Create a new project</Typography>
                     <TextField
+                        inputProps={{ maxLength: 255 }}
+                        required
+                        id="project-name"
+                        label="Project Name"
+                        inputMode="text"
+                        fullWidth={true}
+                    />
+                    <TextField
+                        inputProps={{ maxLength: 5 }}
+                        required
+                        id="project-code"
+                        label="Project Code"
+                        inputMode="text"
+                        fullWidth={true}
+                    />
+                    <TextField
+                        inputProps={{ maxLength: 255 }}
                         required
                         id="jira-link"
                         label="Jira Link"
+                        inputMode="url"
                         fullWidth={true}
                     />
                     <TextField
+                        inputProps={{ maxLength: 255 }}
                         id="confluence-link"
                         label="Confluence Link"
+                        inputMode="url"
                         fullWidth={true}
                     />
                     <TextField
+                        inputProps={{ maxLength: 255 }}
                         id="github-link"
                         label="Github Link"
+                        inputMode="url"
                         fullWidth={true}
                     />
                     <Button variant="contained" color="primary" type="submit" size="large">
@@ -30,7 +52,6 @@ function CreateProject() {
                 </Stack>
             </Container>
         </div>
-        
     );
 }
 
--- project-tracker-frontend/src/components/navbar/navbar.jsx
+++ project-tracker-frontend/src/components/navbar/navbar.jsx
@@ -1,18 +1,31 @@
 import './navbar.css'
-import { Switch, Button, Typography, Stack } from '@mui/material';
+import { Switch, Button, Typography, Stack, Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import logo from "../../assets/logo.svg";
+import React, { useState } from 'react';
+
 function NavBar({ darkMode, toggleDarkTheme, func }) {
 
     const theme = useTheme();
     var isAdmin = true;
 
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
+        <React.Fragment>
             <Stack direction={'row'} spacing={3} alignItems={'center'} justifyContent={'center'} className="navbar">
-                <img src={logo} className="logo" alt="Logo" onClick={() => func(-1)}/>
+                <img src={logo} className="logo" alt="Logo" onClick={() => func(-1)} />
                 {isAdmin ? (
                     <>
-                        <Button variant="contained" color="secondary" type="submit" size="medium">Edit Project</Button>
+                        <Button variant="contained" color="secondary" type="submit" size="medium" onClick={handleClickOpen}>Edit Project</Button>
                         <Button variant="contained" color="secondary" type="submit" size="medium">Edit Members</Button>
                         <Button variant="contained" color="secondary" type="submit" size="medium">Edit Links</Button>
                     </>
@@ -24,11 +37,49 @@ function NavBar({ darkMode, toggleDarkTheme, func }) {
                     Logout
                 </Button>
             </Stack>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                PaperProps={{
+                    component: 'form',
+                    onSubmit: (event) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries(formData.entries());
+                        const project_name = formJson.project_name;
+                        console.log(project_name);
+                        handleClose();
+                    },
+                }}
+            >
+                <DialogTitle>Edit Project</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please fill in any fields you would like to update.
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="project_name"
+                        name="project_name"
+                        label="Project Name"
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button type="submit">Save</Button>
+                </DialogActions>
+            </Dialog>
+        </React.Fragment>
     )
 }
 
-NavBar.defaultProps = {func :() => {console.log('No function provided')}}
+NavBar.defaultProps = { func: () => { console.log('No function provided') } }
 
-NavBar.propTypes = {func : Function}
+NavBar.propTypes = { func: Function }
 
 export default NavBar
