dd89ff9f992d5510d4aea636dd07f00eb4263e4c
Joseph Kimathi <joseph.kimathi@bbd.co.za>
2024-06-02 19:51:11 +0200

    login is working

---
+++ project-tracker-frontend/src/components/buttons/login-button.jsx
@@ -0,0 +1,36 @@
+// src/Button.js
+import React from 'react';
+import Button from "@mui/material/Button";
+import './login-button.css'; // Optional: For styling the button
+
+const loginURL = `https://test-project.auth.eu-west-1.amazoncognito.com/oauth2/authorize?client_id=1echqqb1svir38d3quu5qsu63r&response_type=token&scope=email+openid+profile&attributes=name+email+username+nickname&redirect_uri=http://${window.location.host}/login`;
+
+function handleClick() {
+  window.location.href = loginURL;
+}
+
+const LoginButton = () => {
+  return (
+    <Button color="info" onClick={handleClick} id="login-button" size='large'>Login With Google</Button>
+  );
+};
+
+export default LoginButton;
+
+if (location.hash.includes("access_token")) {
+  let hash = location.hash.substring(1);
+  window.history.replaceState(null, null, ' ');
+  let fragmentParams = new URLSearchParams(hash);
+  const accessToken = fragmentParams.get("access_token");
+  const idToken = fragmentParams.get("id_token");
+  sessionStorage.setItem("idToken", idToken);
+  sessionStorage.setItem("token", accessToken);
+  loginUser()
+}
+
+function loginUser() {
+  // let resp
+  window.location.href = window.location.protocol+"//"+window.location.host;
+  console.log(window.location.protocol+"//"+window.location.host);
+
+}
