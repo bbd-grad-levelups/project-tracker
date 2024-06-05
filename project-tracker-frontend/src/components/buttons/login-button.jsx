69136dcca2f2dbf9d8adf4cbc09565ed62001503
Daniel Pienaar <daniel.pienaar@bbd.co.za>
2024-06-05 11:40:16 +0200

    fix some console errors

--- project-tracker-frontend/src/components/buttons/login-button.jsx
+++ project-tracker-frontend/src/components/buttons/login-button.jsx
@@ -17,10 +17,6 @@ const LoginButton = () => {
           color: 'white',
         },
       }}
-      titleStyle={{
-        color: "white",
-        fontSize: 16,
-      }}
 
       style={{ height: '3rem', width: '15rem', border: '2px solid', fontSize: '1.2rem' }}
     >Login With Cognito</Button>
